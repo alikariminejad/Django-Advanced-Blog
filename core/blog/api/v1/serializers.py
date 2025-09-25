1820c950bbb24116328b627f68ac3baea888a7fe
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2025-09-25 19:20:51 +0330

    added post method to save a post object

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -9,5 +9,5 @@ from ...models import Post
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ["id", "title", "content", "status","created_date", "published_date"]
+        fields = ["id", "title", "author", "content", "status","created_date", "published_date"]
 

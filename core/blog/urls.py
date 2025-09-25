d99b6c3bbe89a16e4059ae4bf86c0338be10f0a5
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2025-09-25 15:22:31 +0330

    api versioning in drf

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from django.views.generic import TemplateView
 from django.views.generic.base import RedirectView
 from . import views
@@ -12,10 +12,10 @@ urlpatterns = [
     # path("go-to-maktabkhooneh", RedirectView.as_view(url='https://www.maktabkhooneh.org'), name="redirect-to-maktabkhooneh"),
     # path('go-to-index', RedirectView.as_view(pattern_name='blog:cbv-index'), name='redirect-to-index'),
     # path('go-to-maktabkhooneh/<int:pk>/', views.RedirectToMaktab.as_view(), name="redirect-to-maktabkhooneh"),  
-    # path('post/', views.PostList.as_view(), name='post-list'),
-    # path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
-    # path('post/create/', views.PostCreateView.as_view(), name='post-create'),
-    # path('post/<int:pk>/edit/', views.PostEditView.as_view(), name='post-edit'),
-    # path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
-    path('post/', views.api_post_list_view, name="api-post-list"),
+    path('post/', views.PostList.as_view(), name='post-list'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+    path('post/create/', views.PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/edit/', views.PostEditView.as_view(), name='post-edit'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
+    path('api/v1/', include('blog.api.v1.urls')),
 ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -5,9 +5,7 @@ from django.views.generic import ListView, DetailView, FormView, CreateView, Upd
 from .models import Post
 from django.shortcuts import get_object_or_404
 from .forms import PostForm
-from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin  
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
 # a function based view to show index page
 """
@@ -94,7 +92,3 @@ class PostEditView(LoginRequiredMixin,UpdateView):
 class PostDeleteView(LoginRequiredMixin,DeleteView):
     model = Post
     success_url = '/blog/post/'
-    
-@api_view(["GET"])
-def api_post_list_view(request):
-    return Response("ok")

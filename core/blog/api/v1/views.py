381c95c91d6efa6df48a14a7e053ba440b23c85c
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2025-09-29 11:57:43 +0330

    testing authentication in drf

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -1,11 +1,13 @@
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from .serializers import PostSerializer
 from ...models import Post
 from django.shortcuts import get_object_or_404
 
 @api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticatedOrReadOnly])
 def postList(request):
     if request.method == 'GET':
         posts = Post.objects.filter(status=True)
@@ -18,6 +20,7 @@ def postList(request):
         return Response(serializer.data)
 
 @api_view(['GET', 'PUT', 'DELETE'])
+@permission_classes([IsAuthenticatedOrReadOnly])
 def postDetail(request, id):
     # try:
     #     post = Post.objects.get(pk=id)

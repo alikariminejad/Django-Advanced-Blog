aa45615a96cffc05a8ec5647276ba5994ece542c
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2025-09-30 21:07:21 +0330

    using DefaultRouter from rest_framework routers

--- core/blog/api/v1/urls.py
+++ core/blog/api/v1/urls.py
@@ -1,14 +1,18 @@
 from django.urls import path, include
 from . import views
+from rest_framework.routers import DefaultRouter
 
 app_name = 'api-v1'
+router = DefaultRouter()
+router.register('post', views.PostViewSet, 'post')
 
+urlpatterns = router.urls
 
-urlpatterns = [
-    # path('post/', views.postList, name='post-list'),
-    # path('post/', views.PostList.as_view(), name='post-list'),
-    # path('post/<int:id>/', views.postDetail, name='post-detail'),
-    # path('post/<int:id>/', views.PostDetail.as_view(), name='post-detail'),
-    path('post/', views.PostViewSet.as_view({'get':'list', 'post':'create'}), name='post-list'),
-    path('post/<int:pk>/', views.PostViewSet.as_view({'get':'retrieve', 'put':'update','patch':'partial_update', 'delete':'destroy'}), name='post-detail'),
-]
+# urlpatterns = [
+#     # path('post/', views.postList, name='post-list'),
+#     # path('post/', views.PostList.as_view(), name='post-list'),
+#     # path('post/<int:id>/', views.postDetail, name='post-detail'),
+#     # path('post/<int:id>/', views.PostDetail.as_view(), name='post-detail'),
+#     path('post/', views.PostViewSet.as_view({'get':'list', 'post':'create'}), name='post-list'),
+#     path('post/<int:pk>/', views.PostViewSet.as_view({'get':'retrieve', 'put':'update','patch':'partial_update', 'delete':'destroy'}), name='post-detail'),
+# ]

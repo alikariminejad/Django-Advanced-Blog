f98c5d332e74caffbb1ff30f15275c2fbb0fd541
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2025-10-03 16:21:59 +0330

    added ordering filter in views

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -10,7 +10,7 @@ from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIV
 from rest_framework import viewsets
 from .permissions import IsOwnerOrReadOnly
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 """@api_view(['GET', 'POST'])
 @permission_classes([IsAuthenticatedOrReadOnly])
@@ -107,9 +107,10 @@ class PostModelViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
     serializer_class = PostSerializer
     queryset = Post.objects.filter(status=True)
-    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_fields = ['category', 'author', 'status']
     search_fields = ['title', 'content']
+    ordering_fields = ['published_date']
 
 class CategoryModelViewSet(viewsets.ModelViewSet):
     """getting a list of categories and creating new categories"""

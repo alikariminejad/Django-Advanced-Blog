be056948b69c52eb05e36e771ccb805afb7494bb
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2025-10-05 09:21:02 +0330

    added output.json url for exporting apis and documentations

--- core/core/urls.py
+++ core/core/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('blog/', include('blog.urls')),
     path('api-docs/', include_docs_urls(title='api sample')),
-    # re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/output.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]

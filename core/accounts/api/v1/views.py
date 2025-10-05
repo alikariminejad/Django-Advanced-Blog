b40296073049c24ca8bfd9aa2bb7dceff4aaae12
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2025-10-05 11:03:32 +0330

    added validation to Registration API

--- core/accounts/api/v1/views.py
+++ core/accounts/api/v1/views.py
@@ -14,4 +14,5 @@ class RegistrationApiView(generics.GenericAPIView):
             data = {
                 'email': serializer.validated_data['email']
             }
-        return Response(data, status=status.HTTP_201_CREATED)
+            return Response(data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)

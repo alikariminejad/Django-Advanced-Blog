88729665ddde41cf041eba2dca438e57f59ebac8
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2025-10-06 11:50:50 +0330

    separating different urls

--- core/accounts/api/v1/urls/accounts.py
+++ core/accounts/api/v1/urls/accounts.py
@@ -1,12 +1,9 @@
 from django.urls import path, include
-from . import views
+from .. import views
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
     TokenRefreshView,
     TokenVerifyView
 )
-
-app_name = "api-v1"
 
 urlpatterns = [
     # registration
@@ -21,6 +18,4 @@ urlpatterns = [
     path('jwt/create/', views.CustomTokenObtainPairView.as_view(), name='jwt-create'),
     path('jwt/refresh/', TokenRefreshView.as_view(), name='jwt-refresh'),
     path('jwt/verify/', TokenVerifyView.as_view(), name='jwt-verify'),
-    # profile
-    path('profile/',views.ProfileApiView.as_view(),name="profile"),
 ]

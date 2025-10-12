36622ca51308f6c8bc3c767fd263076bfa2a884a
alikariminejad <kariminejad.ali.ak@gmail.com>
2025-10-12 19:15:33 +0330

    sending email using threding

--- core/accounts/api/v1/views.py
+++ core/accounts/api/v1/views.py
@@ -11,8 +11,8 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.views import TokenObtainPairView
 from django.contrib.auth import get_user_model
 from ...models import Profile
-from django.core.mail import send_mail
-from mail_templated import send_mail
+from mail_templated import EmailMessage
+from ..utils import EmailThread
 
 User = get_user_model()
 
@@ -85,5 +85,6 @@ class ProfileApiView(generics.RetrieveUpdateAPIView):
     
 class TestEmailSend(generics.GenericAPIView):
     def get(self, request, *args, **kwargs):
-        send_mail('email/hello.tpl', {'name':'ali'}, 'admin@admin.com', {'kariminejad.ali.ak@gmail.com'})
+        email_obj = EmailMessage('email/hello.tpl', {'name':'ali'}, 'admin@admin.com', to={'kariminejad.ali.ak@gmail.com'})
+        EmailThread(email_obj).start()
         return Response("Email sent")

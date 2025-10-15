45374c545ee53f99fc839c4915affa559c22be9f
alikariminejad <kariminejad.ali.ak@gmail.com>
2025-10-15 11:42:14 +0330

    categorized tests and test urls

---
+++ core/blog/tests/test_blog_urls.py
@@ -0,0 +1,16 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from ..views import IndexView, PostList,PostDetailView
+
+class TestUrl(SimpleTestCase):
+    def test_blog_index_url_resolve(self):
+        url = reverse('blog:index')
+        self.assertEquals(resolve(url).func.view_class, IndexView)
+
+    def test_blog_post_list_url_resolve(self):
+        url = reverse('blog:post-list')
+        self.assertEquals(resolve(url).func.view_class,PostList)
+
+    def test_blog_post_detail_url_resolve(self):
+        url = reverse('blog:post-detail', kwargs={'pk':1})
+        self.assertEquals(resolve(url).func.view_class, PostDetailView)

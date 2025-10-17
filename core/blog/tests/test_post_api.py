3fcff4408558c339f7797f3e4db3bd6af475b081
alikariminejad <kariminejad.ali.ak@gmail.com>
2025-10-17 23:09:04 +0330

    post request test

--- core/blog/tests/test_post_api.py
+++ core/blog/tests/test_post_api.py
@@ -1,3 +1,4 @@
+import datetime
 from django.urls import reverse, resolve
 from rest_framework.test import APIClient
 import pytest
@@ -9,3 +10,14 @@ class TestPostApi:
         url = reverse("blog:api-v1:post-list")
         response = client.get(url)
         assert response.status_code == 200
+    
+    def test_create_post_response_401_status(self):
+        url = reverse("blog:api-v1:post-list")
+        data = {
+            'title':'test',
+            'content':'description',
+            'status':True,
+            'published_date':datetime.now()
+        }
+        response = self.client.post(url,data)
+        assert response.status_code == 401

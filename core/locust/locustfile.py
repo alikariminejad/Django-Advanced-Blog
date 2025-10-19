8297c545ed6586f515bf71ee1bb7593a3afc0277
alikariminejad <kariminejad.ali.ak@gmail.com>
2025-10-19 12:28:46 +0330

    added user authentication header to tasks

--- core/locust/locustfile.py
+++ core/locust/locustfile.py
@@ -3,6 +3,10 @@ from locust import HttpUser, task, between
 
 class QuickstartUser(HttpUser):
     
+    def on_start(self):
+        response = self.client.post('/accounts/api/v2/jwt/create/', data={"email": "ali@test.com", "password":"12345"}).json()
+        self.client.headers = {'Authorization': f'Bearer {response.get('access', None)}'}
+    
     @task
     def post_list(self):
         self.client.get("/blog/api/v1/post/")

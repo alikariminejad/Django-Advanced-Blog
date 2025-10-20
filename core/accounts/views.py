547a7fcfd772c3936e1f028b94b33f52c7616bbb
alikariminejad <kariminejad.ali.ak@gmail.com>
2025-10-20 11:44:58 +0330

    caching information

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,9 +1,12 @@
 from django.http import HttpResponse, JsonResponse
 import time
+from django.core.cache import cache
 from .tasks import sendEmail
 import requests
 
 
 def test(request):
-    response = requests.get("")
-    return JsonResponse(response.json())
+    if cache.get("test_delay_api") is None:
+        response = requests.get("")
+        cache.set("test_delay_api", response.json(),60)
+    return JsonResponse(cache.get("test_delay_api"))
